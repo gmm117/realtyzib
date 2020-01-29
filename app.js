dd6d0adccd968ccd6318e86d93c8ffc26fa65ac9
hong <gmm117@naver.com>
2020-01-29 23:34:54 +0900

    heroku connect & build upload

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post('/api/call', function(req,res) {
   });
 });
 
-app.listen(8000, () => {
-  console.log('listening on port 8000!')
+app.listen(process.env.PORT || 3000, () => {
+  console.log('listening on port' + process.env.PORT + '!');
 });
 
